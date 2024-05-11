b3a52edd29452806c95dea40e156a587500ad511
Jurica Runtas <runtas.j@gmail.com>
2024-05-11 10:04:55 +0200

    update run script

--- run.sh
+++ run.sh
@@ -3,8 +3,14 @@
 N=10
 MODEL_NAME=DeepLabV3+_WideResNet38
 IO=/media/jurica/c2d76687-c280-496a-91dc-acee1afa83ab1/io/$MODEL_NAME
-LOG_FOLDER_LAF=$IO/laf_eval/logs
-LOG_FOLDER_FS=$IO/fs_eval/logs
+IO_LAF=$IO/laf_eval
+IO_FS=$IO/fs_eval
+
+LOG_FOLDER_LAF=$IO_LAF/logs
+LOG_FOLDER_FS=$FS_LAF/logs
+
+RESULTS_FOLDER_LAF=$IO_LAF/results/entropy_counts_per_pixel
+RESULTS_FOLDER_FS=$IO_FS/results/entropy_counts_per_pixel
 
 mkdir -p $LOG_FOLDER_LAF
 mkdir -p $LOG_FOLDER_FS
@@ -19,5 +25,8 @@ for ((i=1; i<=N; ++i)); do
   python3 evaluation.py --pixel_eval | tee $LOG_FOLDER_LAF/$i/pixel_evaluation.txt
   python3 evaluation.py --pixel_eval --VALSET=Fishyscapes | tee $LOG_FOLDER_LAF/$i/pixel_evaluation.txt
   python3 cityscapes_valset_eval.py --TRAIN_NUM=$i | tee
+  
+  find $RESULTS_FOLDER_LAF -name '*.p' -delete
+  find $RESULTS_FOLDER_FS -name '*.p' -delete
 done
 
