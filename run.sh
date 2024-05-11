3db3b728c00a28017059e32906818091c4eedf5e
Jurica Runtas <runtas.j@gmail.com>
2024-05-11 20:14:34 +0000

    final run

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 N=10
 MODEL_NAME=DeepLabV3+_WideResNet38
-IO=/media/jurica/c2d76687-c280-496a-91dc-acee1afa83ab1/io/$MODEL_NAME
+IO=/io/$MODEL_NAME
 IO_LAF=$IO/laf_eval
 IO_FS=$IO/fs_eval
 
 LOG_FOLDER_LAF=$IO_LAF/logs
-LOG_FOLDER_FS=$FS_LAF/logs
+LOG_FOLDER_FS=$IO_FS/logs
 
 RESULTS_FOLDER_LAF=$IO_LAF/results/entropy_counts_per_pixel
 RESULTS_FOLDER_FS=$IO_FS/results/entropy_counts_per_pixel
@@ -20,8 +20,8 @@ for ((i=1; i<=N; ++i)); do
   mkdir -p $LOG_FOLDER_FS/$i
 
   python3 ood_training.py | tee $LOG_FOLDER_LAF/$i/train_output.txt
-  python3 meta_classification.py --metaseg_prepare | tee
-  python3 meta_classification.py --metaseg_prepare --VALSET=Fishyscapes | tee
+  #python3 meta_classification.py --metaseg_prepare | tee
+  #python3 meta_classification.py --metaseg_prepare --VALSET=Fishyscapes | tee
   python3 evaluation.py --pixel_eval | tee $LOG_FOLDER_LAF/$i/pixel_evaluation.txt
   python3 evaluation.py --pixel_eval --VALSET=Fishyscapes | tee $LOG_FOLDER_FS/$i/pixel_evaluation.txt
   python3 cityscapes_valset_eval.py --TRAIN_NUM=$i | tee
