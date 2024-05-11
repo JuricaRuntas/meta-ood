a1134ea449b7acb735725e85cf31fe4f6fc223c0
Jurica Runtas <runtas.j@gmail.com>
2024-05-11 08:26:45 +0000

    fix run

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ for ((i=1; i<=N; ++i)); do
   python3 meta_classification.py --metaseg_prepare | tee
   python3 meta_classification.py --metaseg_prepare --VALSET=Fishyscapes | tee
   python3 evaluation.py --pixel_eval | tee $LOG_FOLDER_LAF/$i/pixel_evaluation.txt
-  python3 evaluation.py --pixel_eval --VALSET=Fishyscapes | tee $LOG_FOLDER_LAF/$i/pixel_evaluation.txt
+  python3 evaluation.py --pixel_eval --VALSET=Fishyscapes | tee $LOG_FOLDER_FS/$i/pixel_evaluation.txt
   python3 cityscapes_valset_eval.py --TRAIN_NUM=$i | tee
   
   find $RESULTS_FOLDER_LAF -name '*.p' -delete
