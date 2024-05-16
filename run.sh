5c307c9bf419ea187b1d36ef6641e5630727b7b9
Jurica Runtas <runtas.j@gmail.com>
2024-05-16 22:56:28 +0200

    update run

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 N=10
 MODEL_NAME=DeepLabV3+_WideResNet38
-IO=/io/$MODEL_NAME
+IO=/media/jurica/c2d76687-c280-496a-91dc-acee1afa83ab1/io/$MODEL_NAME
 IO_LAF=$IO/laf_eval
 IO_FS=$IO/fs_eval
 
@@ -12,6 +12,9 @@ LOG_FOLDER_FS=$IO_FS/logs
 RESULTS_FOLDER_LAF=$IO_LAF/results/entropy_counts_per_pixel
 RESULTS_FOLDER_FS=$IO_FS/results/entropy_counts_per_pixel
 
+METRICS_IO_FOLDER_LAF=$IO_LAF/metaseg_io/metrics/epoch_4_alpha_0.9_t0.7
+METRICS_IO_FOLDER_FS=$IO_FS/metaseg_io/metrics/epoch_4_alpha_0.9_t0.7
+
 mkdir -p $LOG_FOLDER_LAF
 mkdir -p $LOG_FOLDER_FS
 
@@ -20,12 +23,20 @@ for ((i=1; i<=N; ++i)); do
   mkdir -p $LOG_FOLDER_FS/$i
 
   python3 ood_training.py | tee $LOG_FOLDER_LAF/$i/train_output.txt
+  
   #python3 meta_classification.py --metaseg_prepare | tee
   #python3 meta_classification.py --metaseg_prepare --VALSET=Fishyscapes | tee
+
+  #cp $METRICS_IO_FOLDER_LAF/meta_classifier_predictions.p $LOG_FOLDER_LAF/$i/
+  #cp $METRICS_IO_FOLDER_FS/meta_classifier_predictions.p $LOG_FOLDER_FS/$i/
+
   python3 evaluation.py --pixel_eval | tee $LOG_FOLDER_LAF/$i/pixel_evaluation.txt
   python3 evaluation.py --pixel_eval --VALSET=Fishyscapes | tee $LOG_FOLDER_FS/$i/pixel_evaluation.txt
   python3 cityscapes_valset_eval.py --TRAIN_NUM=$i | tee
   
+  cp $RESULTS_FOLDER_LAF/epoch_4_alpha_0.9.p $LOG_FOLDER_LAF/$i/
+  cp $RESULTS_FOLDER_FS/epoch_4_alpha_0.9.p $LOG_FOLDER_FS/$i/
+
   find $RESULTS_FOLDER_LAF -name '*.p' -delete
   find $RESULTS_FOLDER_FS -name '*.p' -delete
 done
